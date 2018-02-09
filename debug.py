b8e657ba05004794823ff2b83012cc36846a3dda
Steve C <diceroll123@gmail.com>
2018-02-09 16:54:00 -0500

    Fix formatting + exception type in scrape_emoji command

--- debug.py
+++ debug.py
@@ -14,25 +14,29 @@ class Debug:
 
         memfile = io.BytesIO()
         zip = zipfile.ZipFile(memfile, 'w')
+        emoji_session = aiohttp.ClientSession(loop=ctx.bot.loop)
 
         with ctx.typing():
             for guild in ctx.bot.guilds:
                 guildname = guild.name.replace("/", "") # may require more replacing
                 for emoji in guild.emojis:
-                    async with aiohttp.ClientSession(loop=ctx.bot.loop).get(emoji.url) as resp:
+                    async with emoji_session.get(emoji.url) as resp:
                         if resp.status == 200:
                             data = await resp.read()
                             extension = emoji.url.split('.')[-1]
-                            emojiname = 'emojis/{guildname}/{emoji.name}.{extension}'.format(guildname=guildname, emoji=emoji, extension=extension)
+                            emojiname = 'emojis/{guildname}/{emoji.name}.{extension}'.format(
+                                guildname=guildname, emoji=emoji, extension=extension)
 
                             # check if this guild has multiple of the same emoji name, and rename them accordingly
                             multiple_num = 0
                             while emojiname in zip.namelist():
                                 multiple_num += 1
-                                emojiname = 'emojis/{guildname}/{emoji.name}~{multiple_num}.{extension}'.format(guildname=guildname, emoji=emoji, multiple_num=multiple_num, extension=extension)
+                                emojiname = 'emojis/{guildname}/{emoji.name}~{multiple_num}.{extension}'.format(
+                                    guildname=guildname, emoji=emoji, multiple_num=multiple_num, extension=extension)
                             zip.writestr(emojiname, data)
                         else:
-                            raise commands.BadArgument('Received error code {error_id} while fetching emojis.'.format(error_id=resp.status))
+                            raise Exception(
+                                'Received error code {error_id} while fetching emojis.'.format(error_id=resp.status))
         zip.close()
 
         memfile.seek(0)
